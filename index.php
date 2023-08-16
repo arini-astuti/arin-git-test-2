122e15df7af4bd3261e588b40f9cb3d5148500d0
arini-astuti <ariniastuti273@gmail.com>
2023-08-16 12:34:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat qui, molestiae quasi doloremque quae repellendus nam minus animi praesentium reiciendis! Omnis voluptatum voluptatibus ratione est laudantium explicabo recusandae, blanditiis deserunt!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
